e6fd3db067e7c0e8233a5115801679f7f30fb4e9
Desi Sandor <d.alex.galaxy@gmail.com>
2019-10-07 16:18:54 +0100

    Updating score

--- app.js
+++ app.js
@@ -44,6 +44,36 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
 
 
 	//3. update the round score If the rolled number was not a 1 
+
+	if (dice !== 1) {
+	roundScore += dice;
+	document.querySelector('#current-' + activePlayer).textContent = roundScore;  
+		//add score
+	} else {
+		activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+		roundScore = 0;
+
+		document.getElementById('current-0').textContent = '0'
+		document.getElementById('current-1').textContent = '0'
+
+		document.querySelector('.player-0-panel').classList.toggle('active');
+		document.querySelector('.player-1-panel').classList.toggle('active');
+		document.querySelector('.dice').style.display = 'none';
+
+
+
+
+
+
+		//document.querySelector('.player-0-panel').classList.remove('active');
+		//document.querySelector('.player-1-panel').classList.add('active');
+
+
+
+		//next player
+	}
+
+
 });
 
 
