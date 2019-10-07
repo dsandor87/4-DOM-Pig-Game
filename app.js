cb2529869fc50d6d5765c444581a1e4b645e4192
Desi Sandor <d.alex.galaxy@gmail.com>
2019-10-07 22:50:58 +0100

    Update app.js
    Init function

--- app.js
+++ app.js
@@ -9,25 +9,19 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
+init();
+
+
 
 dice = Math.floor(Math.random()*6)+1;
 console.log(dice);
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).textContent = dice;
 
 
 
-document.querySelector('.dice').style.display = 'none'
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
 
 
 document.querySelector('.btn-roll').addEventListener('click', function(){ 
@@ -85,6 +79,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
 		document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
 		document.querySelector('.player-'+ activePlayer +'-panel').classList.remove('active');
 
+
 	} else{nextPlayer()}
 
 	//Next Player 
@@ -104,6 +99,32 @@ function nextPlayer() {
 		document.querySelector('.dice').style.display = 'none';
 
 }
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init() {
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+document.querySelector('.dice').style.display = 'none'
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+document.getElementById('name-0').textContent = 'Player 1';
+document.getElementById('name-1').textContent = 'Player 2';
+document.querySelector('.player-0-panel').classList.remove('winner');
+document.querySelector('.player-1-panel').classList.remove('winner');
+document.querySelector('.player-0-panel').classList.remove('active');
+document.querySelector('.player-1-panel').classList.remove('active');
+document.querySelector('.player-0-panel').classList.add('active');
+
+
+
+
+};
 
 
 
